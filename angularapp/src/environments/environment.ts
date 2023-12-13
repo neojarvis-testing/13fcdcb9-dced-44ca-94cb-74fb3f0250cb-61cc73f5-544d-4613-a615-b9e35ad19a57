75fe56a87173ec233a199556c4f45ab5eb36d508
KishorkumarKR-iamneo <kishorkumar.kr@iamneo.ai>
2023-12-13 17:06:27 +0000

    hello

--- angularapp/src/environments/environment.ts
+++ angularapp/src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  production1: false,
+  production2: false,
 };
 
 /*
